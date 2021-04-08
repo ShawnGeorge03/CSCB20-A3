6434c23abf64ac176eac79a62b77535511f564c3
agha19 <81888901+agha19@users.noreply.github.com>
2021-04-07 22:06:44 -0400

    comments

--- app.py
+++ app.py
@@ -200,95 +200,122 @@ def logout():
     return redirect(url_for('login'))
 
 def get_student_grades(username):
+    # Creates a list with grades and initially sets all values to not graded yet
     grades = []
     for i in range(5):
         grades.append("Not graded yet")
-    # Creates a list with grades and initially sets all values to not graded yet
+    # Runs a query to find quiz1 mark of a student and adds this value to the list, if the mark is not
     result = query_db('SELECT MAX(quiz1) FROM marks WHERE student_username = ?', [username], one=True)
-    
-    #
+    # If the mark is not present, assigns "Not graded yet", if it is present assigns this value to the first entry of grades list(First quiz)
     grades[0] = result[0] if result[0]  else "Not graded yet"
-    
+    # Runs a query to find quiz2 mark of a student and adds this value to the list, if the mark is not
     result = query_db('SELECT MAX(quiz2) FROM marks WHERE student_username = ?', [username], one=True)
+    # If the mark is not present, assigns "Not graded yet", if it is present assigns this value to the second entry of grades list(Second quiz)
     grades[1] = result[0] if result[0]  else "Not graded yet"
-
+    # Runs a query to find quiz3 mark of a student and adds this value to the list, if the mark is not
     result = query_db('SELECT MAX(quiz3) FROM marks WHERE student_username = ?', [username], one=True)
+    # If the mark is not present, assigns "Not graded yet", if it is present assigns this value to the third entry of grades list(Third quiz)
     grades[2] = result[0] if result[0]  else "Not graded yet"
-
+    # Runs a query to find midterm exam mark of a student and adds this value to the list, if the mark is not
     result = query_db('SELECT MAX(midtermexam) FROM marks WHERE student_username = ?', [username], one=True)
+    # If the mark is not present, assigns "Not graded yet", if it is present assigns this value to the fourth entry of grades list(Midterm)
     grades[3] = result[0] if result[0]  else "Not graded yet"
-
+    # Runs a query to find final exam mark of a student and adds this value to the list, if the mark is not
     result = query_db('SELECT MAX(finalexam) FROM marks WHERE student_username = ?', [username], one=True)
+    # If the mark is not present, assigns "Not graded yet", if it is present assigns this value to the fifth entry of grades list(Final)
     grades[4] = result[0] if result[0] else "Not graded yet"
 
     return grades
     
 def check_if_graded(student):
+    # Selects each mark of a student in the database
     result = query_db('SELECT * FROM marks WHERE student_username = ?', [student], one=False)
+    # Returns true if student data is present in the marks database, false if student is not present in the database 
     return True if result else False
 
 def get_current_username():
+    # Returns username of a current user in session
     result = query_db('SELECT username FROM users WHERE id = ?', args=[session['userID']], one=True)
     return result[0]
 
 def get_current_name():
+      # Returns name of a current user in session
     result = query_db('SELECT name FROM users WHERE id = ?', args=[session['userID']], one=True)
     return result[0]
 
 def get_current_usertype():
+    # Returns usertype of a current user in session
     result = query_db('SELECT usertype FROM users WHERE id = ?', args=[session['userID']], one=True)
-    return result[0]
+    return result[0]   
 
 @app.route("/grades")
 def grades():
+    # check if the user is logged in, if not redirects user to login page
     if 'userID' in session:
+        # Gets current usertype and assigns it to usertype
         usertype = get_current_usertype()
-
+        # If user is a student
         if usertype=="Student":
+            # Renders student grades page after acquiring username and grades of a student
             username = get_current_username()
             grades = get_student_grades(username)
             return render_template('grades_student.html',grades=grades)
+        # If user is an instructor
         elif usertype=='Instructor':
+            # Runs a query to find usernames of each student
             student_list = query_db("SELECT username FROM users WHERE usertype = 'Student'", args=(), one=False)
+            # Runs a query to find username, type of a test and a reason for a remarking request of each student who requested a remark
             r_requests = query_db("SELECT student_username,test,reason FROM requests", args=(), one=False)
-           
+           # Creates an empty dictionary 
             remark_requests = {}
+            # Loops through students who requested a remark 
             for r in r_requests:
+                # If student's username is not already in the remark requests, links the name to the dictionary and creates
+                # a  nested dictionary that consists of student's tests that were requested to be remarked
                 if r[0] not in remark_requests:
                     remark_requests[r[0]] = {}
+                # If a type of a test is not present in the nested dictionary, links the type of a test to a student username ( nested dictionary )
                 if r[1] not in remark_requests[r[0]]:
                     remark_requests[r[0]][r[1]] = []
+                # Assigns the reason of the remarking request to the type of quiz that student requested to be remarked
                 remark_requests[r[0]][r[1]].append(r[2])
-
+            # Creates an empty dicitonary that will be filled with each student's marks
             mark_book = {}
+            # Loops through each student, and assigns grades to the student in the list
             for s in student_list:
                 grades = get_student_grades(s[0])
                 mark_book[s[0]] = grades
             
            
 
-
+            # Renders the page for teachers
             return render_template('grades_teacher.html',mark_book=mark_book,remark_requests=remark_requests)
-
+    # Renders login page if user is not logged in
     return redirect(url_for('login'))
 
 @app.route("/remark", methods=['POST'])
 def request_remark():
+    # If student sends a remark request
     if request.method == 'POST':
+        # Collects student's username, type of a test that should be regraded and a reason for regrading
         username = get_current_username()
         test = request.form['test']
         reason = request.form['reason']
-
+        # Sets up a connection with a database
         db = get_db()
         db.row_factory = make_dicts
+        # Creates a cursor
         cur = db.cursor()
+    # Runs a query to find whether student already requested a remark for a test before
     if query_db('SELECT * FROM requests WHERE student_username = ? AND test = ?', [username,test]):
+        # If student already requested a remark, updates the reason of a request to the latest remark reason
         cur.execute('UPDATE requests SET reason = ? WHERE student_username = ? AND test = ?', [reason,username,test ])
         db.commit()
         cur.close()
-       
+    # When student submits a new remarking request
     else:
-        cur.execute('INSERT INTO requests(student_username,test,reason) VALUES (?, ?, ?)', [username,test,reason])
+        # Inserts student data to the database of remark requests
+        cur.execute('INSERT INTO requests(student_username,test,reason) VALUES (?, ?, ?,)', [username,test,reason])
         db.commit()
         cur.close()
 
